fa943f3742b9bec55f14b2f70d829f013f632b9d
Laura Thomas <lthomas@edcoe.org>
2018-12-16 17:47:26 -0800

    Add first, last name and loosen up password restrictions

--- GitDemo1/Models/IdentityModels.cs
+++ GitDemo1/Models/IdentityModels.cs
@@ -3,6 +3,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
 
 namespace GitDemo1.Models
 {
@@ -16,6 +17,11 @@ namespace GitDemo1.Models
             // Add custom user claims here
             return userIdentity;
         }
+
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
